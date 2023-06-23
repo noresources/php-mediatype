0ebd0fc9cb27bef4f937609293559eac65eb5ec1
Renaud Guillard <dev@nore.fr>
2023-06-23 22:55:40 +0200

    Tests: compare MediaTypeFactory method and MediaType/MediaRange createFromString class methods

--- tests/MediaTypeFactoryTest.php
+++ tests/MediaTypeFactoryTest.php
@@ -7,6 +7,8 @@
  */
 namespace NoreSources\MediaType\Test;
 
+use NoreSources\MediaType\MediaRange;
+use NoreSources\MediaType\MediaType;
 use NoreSources\MediaType\MediaTypeFactory;
 use NoreSources\MediaType\MediaTypeFactoryAwareInterface;
 use NoreSources\MediaType\Traits\MediaTypeFactoryAwareTrait;
@@ -19,7 +21,7 @@ class Aware implements MediaTypeFactoryAwareInterface
 class MediaTypeFactoryTest extends \PHPUnit\Framework\TestCase
 {
 
-	function testAware()
+	public function testAware()
 	{
 		$dflt = MediaTypeFactory::getInstance();
 		$aware = new Aware();
@@ -28,5 +30,48 @@ class MediaTypeFactoryTest extends \PHPUnit\Framework\TestCase
 		$aware->setMediaTypeFactory($dflt);
 		$this->assertEquals($dflt, $aware->getMediaTypeFactory(),
 			'Aware factory set');
+	}
+
+	public function testFromString()
+	{
+		$stringRepresentation = 'application/json; style=json; charset=utf-8';
+		$mediaTypeStaticMethod = MediaType::createFromString(
+			$stringRepresentation, true);
+
+		$this->assertEquals($stringRepresentation,
+			$mediaTypeStaticMethod->jsonSerialize(), 'Media type method');
+
+		$mediaRangeStaticMethod = MediaRange::createFromString(
+			$stringRepresentation, true);
+
+		$this->assertEquals($stringRepresentation,
+			$mediaRangeStaticMethod->jsonSerialize(),
+			'Media range method');
+
+		$factoryMethod = MediaTypeFactory::getInstance()->createFromString(
+			$stringRepresentation, true);
+		$this->assertEquals($stringRepresentation,
+			$factoryMethod->jsonSerialize(), 'Factory method');
+
+		foreach ([
+			'media type vs media range' => [
+				$mediaTypeStaticMethod,
+				$mediaTypeStaticMethod
+			],
+			'media type vs factory' => [
+				$mediaTypeStaticMethod,
+				$factoryMethod
+			],
+			'factory vs media range' => [
+				$factoryMethod,
+				$mediaRangeStaticMethod
+			]
+		] as $label => $pair)
+		{
+			list ($actual, $expected) = $pair;
+
+			$this->assertEquals($actual->jsonSerialize(),
+				$expected->jsonSerialize(), $label);
+		}
 	}
 }
